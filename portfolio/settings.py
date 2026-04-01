c0819ec036f1112d6a15fca2dedce72d35e58772
Alefenba182 <pc.alefe@outlook.com>
2026-04-01 17:03:59 -0300

    Adcionando o Cloudinary como Default_file_storage

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,13 +132,7 @@ CLOUDINARY_STORAGE = {
     'API_KEY': config('CLOUDINARY_API_KEY'),
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
-STORAGES = {
-    "default": {
-        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
-}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
