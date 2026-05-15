d8f48c3c57c747c2f54b81089e4b41ec875a13ac
Alefenba182 <pc.alefe@outlook.com>
2026-05-15 19:30:43 -0300

    atualizando build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --no-input || cp -r static/. staticfiles_collected/
+python manage.py createsuperuser --noinput
